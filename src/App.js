2d0d9c2102d7391b47861931038537542793e9af
Xursand Dev <xursanddev@gmail.com>
2022-07-17 12:04:35 +0300

    first electron tasbeeh

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Wrapper from "./wrapper";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Wrapper />
+      </div>
+    );
+  }
+}
+
+export default App;
